9d476cc0b12cc209574234a45a104b4792ce263b
r-s-coding <112776326+r-s-coding@users.noreply.github.com>
2023-05-07 21:58:39 -0400

    Added website

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    // Retrieve last clicked link from localStorage
+    var lastClickedLink = localStorage.getItem('lastClickedLink');
+
+    // Set active class on last clicked link
+    if (lastClickedLink) {
+        $('#home-link').removeClass('active');
+        $('a.nav-link[href="' + lastClickedLink + '"]').addClass('active');
+    }
+
+    // Set active class on clicked link and store it in localStorage
+    $('a.nav-link').click(function () {
+        $('a.nav-link').removeClass('active');
+        $(this).addClass('active');
+        localStorage.setItem('lastClickedLink', $(this).attr('href'));
+
+        var target = $(this).attr('href').substring($(this).attr('href').indexOf('#'));
+        console.log(target);
+        var targetElem = $(target);
+        console.log(targetElem);
+
+        if (targetElem.length) {
+            $('html, body').animate({
+                scrollTop: targetElem.offset().top - $('.navbar').outerHeight()
+            }, 500);
+        }
+
+        $('.navbar-collapse').collapse('hide');
+    });
+
+}) 
